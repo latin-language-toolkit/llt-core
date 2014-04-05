6fd18d1a773d328917c468fc67358bf351f9e400
LFDM <1986gh@gmail.com>
2014-04-05 13:21:20 +0200

    Create Structures ns and import HashContainable

--- lib/llt/core.rb
+++ lib/llt/core.rb
@@ -2,6 +2,7 @@ require "llt/core/version"
 
 module LLT
   module Core
+    require 'llt/core/structures'
     require 'llt/core/containable'
     require 'llt/core/serviceable'
   end
---
+++ lib/llt/core/structures.rb
@@ -0,0 +1,7 @@
+module LLT
+  module Core
+    module Structures
+      require 'llt/core/structures/hash_containable'
+    end
+  end
+end
