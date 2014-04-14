b4f2733d6937a3ab1ac65a48f91587e1e4860009
LFDM <1986gh@gmail.com>
2014-04-14 12:01:08 +0200

    Update formatting of version routes

--- lib/llt/core/api/version_routes.rb
+++ lib/llt/core/api/version_routes.rb
@@ -1,16 +1,22 @@
+require 'llt/core/api/helpers'
+
 module LLT
-  module Core::Api::VersionRoutes
-    def add_version_route_for(route, params)
-      dependencies = params[:dependencies]
+  module Core::Api
+    module VersionRoutes
+      def add_version_route_for(route, params)
+        dependencies = params[:dependencies]
 
-      get("#{route}/version") do
-        output = dependencies.map do |dep_class|
-          versioner = LLT.const_get(dep_class).const_get(:VersionInfo).new
-          versioner.to_xml
-        end.join
+        get("#{route}/version") do
+          services = dependencies.map do |dep_class|
+            versioner = LLT.const_get(dep_class).const_get(:VersionInfo).new
+            versioner.to_xml
+          end.join
 
-        respond_to do |f|
-          f.xml { output }
+          output = "#{Helpers::XML_DECLARATION}<services>#{services}</services>"
+
+          respond_to do |f|
+            f.xml { output }
+          end
         end
       end
     end
