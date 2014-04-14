fdd64d265ce0d28738c8552587c3b3566c55683b
LFDM <1986gh@gmail.com>
2014-04-14 11:44:50 +0200

    Listen to xml for dynamic version routes

--- lib/llt/core/api/version_routes.rb
+++ lib/llt/core/api/version_routes.rb
@@ -4,10 +4,14 @@ module LLT
       dependencies = params[:dependencies]
 
       get("#{route}/version") do
-        dependencies.map do |dep_class|
+        output = dependencies.map do |dep_class|
           versioner = LLT.const_get(dep_class).const_get(:VersionInfo).new
           versioner.to_xml
         end.join
+
+        respond_to do |f|
+          f.xml { output }
+        end
       end
     end
   end
