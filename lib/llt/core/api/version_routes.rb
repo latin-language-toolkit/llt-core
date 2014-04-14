dc800c7fc45f6e28c6b27417d085b27ccf0a1e1a
LFDM <1986gh@gmail.com>
2014-04-14 11:16:42 +0200

    Build dynamic version route output

--- lib/llt/core/api/version_routes.rb
+++ lib/llt/core/api/version_routes.rb
@@ -3,9 +3,11 @@ module LLT
     def add_version_route_for(route, params)
       dependencies = params[:dependencies]
 
-require 'pry'; binding.pry
-      self.class.get("#{interpolation}/version") do
-
+      get("#{route}/version") do
+        dependencies.map do |dep_class|
+          versioner = LLT.const_get(dep_class).const_get(:Version).new
+          versioner.to_xml
+        end.join
       end
     end
   end
