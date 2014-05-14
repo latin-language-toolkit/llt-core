eb59deb570bb16567377568df3ddfab9bb370398
LFDM <1986gh@gmail.com>
2014-05-14 18:16:46 +0200

    Formally declare module in HashContaibale::Generic
    Easier to require the file on its own then.

--- lib/llt/core/structures/hash_containable/generic.rb
+++ lib/llt/core/structures/hash_containable/generic.rb
@@ -1,22 +1,26 @@
 module LLT
-  module Core::Structures::HashContainable
-    # This class includes HashContainable and should be used as a simplistic
-    # container with a stable tag and mostly without any attributes.
-    # The id serves as tag.
-    #
-    # Thinking of xml this could look like this:
-    # <files>
-    #   # the contents of the containable
-    # </files>
-    class Generic
-      include Core::Structures::HashContainable
+  module Core
+    module Structures
+      module HashContainable
+        # This class includes HashContainable and should be used as a simplistic
+        # container with a stable tag and mostly without any attributes.
+        # The id serves as tag.
+        #
+        # Thinking of xml this could look like this:
+        # <files>
+        #   # the contents of the containable
+        # </files>
+        class Generic
+          include Core::Structures::HashContainable
 
-      def xml_tag
-        @id
-      end
+          def xml_tag
+            @id
+          end
 
-      def xml_attributes
-        {}
+          def xml_attributes
+            {}
+          end
+        end
       end
     end
   end
